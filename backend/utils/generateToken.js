e4762402708d9f2d285538b87024d4fb49aad177
Harshdomadiya <harshdomadiya1014@gmail.com>
2024-03-10 15:14:17 +0530

    final

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-const generateTokenAndSentCookies = (userId,res) =>{
+const 
+generateTokenAndSentCookies = (userId,res) =>{
       const token = jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:'15d'
       })
